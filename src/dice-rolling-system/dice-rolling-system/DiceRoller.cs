805f760d30545b03818072d28ca7a57834607aec
jroscoe5 <36338176+jroscoe5@users.noreply.github.com>
2019-06-11 19:45:34 -0700

    Added DiceRoller for Liam, moved some stuff around

---
+++ src/dice-rolling-system/dice-rolling-system/DiceRoller.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dice_rolling_system
+{
+    static class DiceRoller
+    {
+        static DiceRoller() { }
+        
+        // RNG source - to get an integer use random.Next(inclusive, exclusive);
+        private static Random random = new Random(Guid.NewGuid().GetHashCode());
+
+        // Internal array to keep track of previous roll. Do not return this - return a new array instead.
+        private static int[] currentRolls;
+
+        /// <summary>
+        /// Rolls a given number of 6 sided dice and returns an array of rolls.
+        /// Ex: DiceRoller.Roll(4) -> [3, 1, 6, 2]
+        /// </summary>
+        /// <param name="num">Number of dice to roll.</param>
+        /// <returns>An array of rolls with size equal to num</returns>
+        static public int[] Roll(int num)
+        {
+            return null;
+            // TODO:
+            // Generate and return an array of dice rolls
+            // Store rolls in currentRolls
+        }
+
+        /// <summary>
+        /// Rerolls any number of dice from the previous roll, specified by a list of 
+        /// target indices.
+        /// Ex: currentRolls = [1, 3, 4, 5, 6]; DiceRoller.Reroll([0, 2, 4]) -> [5, 3, 1, 5, 2]
+        /// </summary>
+        /// <param name="targets">Array of target dice to reroll</param>
+        /// <returns>An array of rolls with size equal to previous roll size</returns>
+        static public int[] Reroll(int[] targets)
+        {
+            return null;
+            // TODO: 
+            // Reroll any number of dice from the previous roll and return an array
+            // Store rolls in currentRolls
+        }
+
+        /// <summary>
+        /// Returns a copy of the last roll or reroll if it exists
+        /// </summary>
+        /// <returns>Returns a copy of the last roll or null</returns>
+        static public int[] CurrentRoll()
+        {
+            return (currentRolls == null)? null : (int[])currentRolls.Clone();
+        }
+    }
+}
---
+++ src/dice-rolling-system/dice-rolling-system/TestDriver.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dice_rolling_system
+{
+    class TestDriver
+    {
+        static Random random = new Random(Guid.NewGuid().GetHashCode());
+
+        static void Main(string[] args)
+        {
+            RunTest("CorrectRollCount", CorrectRollCount);
+            RunTest("CorrectRollValues", CorrectRollValues);
+            RunTest("CorrectRollEncapsulation", CorrectRollEncapsulation);
+            RunTest("CorrectRerollEncapsulation", CorrectRerollEncapsulation);
+            RunTest("CorrectRerollFunctionality", CorrectRerollFunctionality);
+            Console.ReadLine();
+        }
+
+        static void RunTest(string name, Func<bool> test)
+        {
+            Console.WriteLine("{0} || Running test {1}", DateTime.Now.ToLongTimeString(), name);
+            try
+            {
+                if (test.Invoke())
+                {
+                    Console.WriteLine("{0} || PASSED", DateTime.Now.ToLongTimeString());
+                }
+                else
+                {
+                    Console.WriteLine("{0} || FAILED", DateTime.Now.ToLongTimeString());
+                }
+            }
+            catch(Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+                Console.WriteLine("{0} || FAILED", DateTime.Now.ToLongTimeString());
+            }
+        }
+
+        /*    TESTS     */
+       
+        static bool CorrectRollCount()
+        {
+            int num = random.Next(1, 9);
+            return DiceRoller.Roll(num).Length == num;
+        }
+
+        static bool CorrectRollValues()
+        {
+            var rolls = DiceRoller.Roll(1000000);
+            foreach (int roll in rolls)
+                if (roll < 1 || roll > 6)
+                    return false;
+            return true;
+        }
+
+        static bool CorrectRollEncapsulation()
+        {
+            var rolls = DiceRoller.Roll(100);
+            rolls[50] = -1;
+            return DiceRoller.CurrentRoll()[50] != -1;
+        }
+
+        static bool CorrectRerollEncapsulation()
+        {
+            DiceRoller.Roll(100);
+            var rolls = DiceRoller.Reroll(new int[]{0, 50, 3});
+            rolls[50] = -1;
+            return DiceRoller.CurrentRoll()[50] != -1;
+        }
+
+        static bool CorrectRerollFunctionality()
+        {
+            var rolls = DiceRoller.Roll(1000000);
+            List<int> evens = new List<int>();
+            for (int i = 0; i < 1000000; i += 2)
+                evens.Add(i);
+            var rerolls = DiceRoller.Reroll(evens.ToArray());
+            for (int i = 1; i < 1000000; i += 2)
+                if(rolls[i] != rerolls[i])
+                    return false;
+            for (int i = 0; i < 1000000; i += 2)
+                if (rolls[i] != rerolls[i])
+                    return true; // at least 1 value was changed
+            return false; // no rerolls happened or REALLY 'lucky'
+        }
+    }
+}
